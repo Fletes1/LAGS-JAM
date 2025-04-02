374353cd939e29a8c09bc403be22263c7b82d99e
EmaArb <emaarbproducciones@gmail.com>
2025-04-02 14:54:33 -0300

    Wwise
    .Volver a integrar los sonidos de la escena 1

--- gj/GeneratedSoundBanks/Wwise_IDs.h
+++ gj/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,6 +13,8 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_AMBIENTE_CATACUMBA = 2655504731U;
+        static const AkUniqueID PLAY_ANTORCHA = 37813828U;
         static const AkUniqueID PLAY_MOUSE_SOBRE_OPCIONES = 3236977634U;
         static const AkUniqueID PLAY_OPCIONES_IN = 4057716190U;
         static const AkUniqueID PLAY_OPCIONES_OFF = 1931507818U;
@@ -27,12 +29,15 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID AMBIENTE = 4095160060U;
+        static const AkUniqueID ESPACIAL = 2460606139U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MENU = 2607556080U;
     } // namespace BUSSES
 
     namespace AUX_BUSSES
     {
+        static const AkUniqueID CATACUMBA = 1182336810U;
         static const AkUniqueID REVERB_MENU = 1031316333U;
     } // namespace AUX_BUSSES
 
